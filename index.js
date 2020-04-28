fee0980119032af8f018015edc6a12b46f0a3233
Brooke Luro <lurob@me.com>
2020-04-28 00:04:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ function decrement(x) {
   return x -= 1
 }
 
-function makeInt(n) {
-  return (parseInt('n', 10))
+function makeInt('n') {
+  return parseInt('n', 10)
 }
 
-function preserveDecimal(n) {
-  return (parseFloat('n'))
+function preserveDecimal('n') {
+  return parseFloat('n')
 }
